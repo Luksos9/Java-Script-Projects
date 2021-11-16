97abdae32c9e8022ef5c564ff815f6f3db027286
Luksos9 <lukaszszumilas9@wp.pl>
2021-11-16 19:30:56 +0100

    added

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-let js = "amazing";
+'use strict'
 
-if (js === "amazing") alert('JavaScript is fun!')
 
-console.log(typeof js)
+// FUnction expression
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+//Arrow function
+const calcAge3 = birthYear => {
+    const age = 2037 - birthYear;
+    const retire = 65 - age;
+    return retire
+};
+
+console.log(calcAge3(2020))
+
