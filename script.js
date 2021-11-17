2b490dedfb88c413f701be62adb87f327f8ba02f
Luksos9 <lukaszszumilas9@wp.pl>
2021-11-17 06:04:14 +0100

    objects

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const yearsUntilRetirement = function (birthYear, firstName) {
 }
 
 console.log(yearsUntilRetirement(2020, 'Lukasz')) */
-
+/* 
 function checkWinner (koala1Score, dolphin1Score) {
     const koalaAvg = calcAverage(koala1Score)
     const dolphinAvg = calcAverage(dolphin1Score)
@@ -53,4 +53,56 @@ const calcAverage = (teamScore) => {
 const dolphinScore = [44, 500, 71]
 const koalaScore = [65, 100, 49]
 
-console.log(checkWinner(koalaScore, dolphinScore))
+console.log(checkWinner(koalaScore, dolphinScore)) */
+
+/* const friends = ['michael', 'steven', 'peter']
+
+const years = new Array(1991, 1992, 1993, 1994)
+
+
+console.log(friends.length)
+console.log(years)
+
+friends[2] = 'Jay-Z'
+console.log(friends)
+
+const a = friends.pop()
+
+console.log(a)
+console.log(friends)
+
+console.log(friends.shift())
+console.log(friends.indexOf('michael'))
+console.log(friends.includes('steven'))
+console.log(friends.includes('steven')) */
+
+/* const calcTip = (bills) => {
+    let tips = []
+    let step = 0
+    let total = []
+    console.log(total)
+    for (step; step <= bills.length-1; step++) {
+        if (bills[step] >= 50 && bills[step] <= 300) {
+            tips.push(0.15*bills[step])
+            total.push(bills[step] + tips[step])
+        } else {
+            tips.push(0.2*bills[step])
+            total.push(bills[step] + tips[step])
+        }
+    }
+    return `For first one, the tip is ${tips[4]} and total value is ${total[4]}\nFor second one, the tip is ${tips[1]} and total value is ${total[1]}\nFor third one, the tip is ${tips[2]} and total value is ${total[2]}`
+} 
+
+const bills = [125, 555, 44, 80, 100]
+
+console.log(calcTip(bills)) */
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    kids: ['Michael', 'Peter', 'Steven']
+}
+
+console.log(jonas)
