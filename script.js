e9b55fe36b5261ea9756cc551b3a9e04ec3c38b8
Luksos9 <lukaszszumilas9@wp.pl>
2021-11-19 06:54:07 +0100

    amplitude calc

--- script.js
+++ script.js
@@ -1,9 +1,60 @@
 'use strict';
 
-const x = 23;
-
-const calcAge = birthYear => {
+/* const x = 23;
+ */
+/* const calcAge = birthYear => {
   cl(`${birthYear} xdaasda`);
 };
 
-console.log(`asdad`);
+console.log(`asdad`); */
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+/* const temperatures1 = [3, -2, -6, -1, 9, 13, 17, 15, 14, 9, 5];
+
+let container = 0;
+
+for (let num = 0; num <= temperatures.length; num++) {
+  if (temperatures[num] !== Number) {
+    continue;
+  } else {
+    if (temperatures[num] > temperatures[num + 1]) {
+      container = temperatures[num];
+    } else {
+      continue;
+    }
+  }
+}
+
+console.log(container);
+ */
+/* 
+const calcTempAplitude = function (templs) {
+  let max = templs[0];
+  for (let i = 1; i < templs.length; ++i) {
+    if (templs[i] > max) {
+      max = templs[i];
+    }
+  }
+  return max;
+};
+ */
+
+const calcTempAplitude = function (temperatures) {
+  let max = temperatures[0];
+  let min = temperatures[0];
+  for (let i = 1; i < temperatures.length; i++) {
+    const curTemp = temperatures[i];
+    if (typeof curTemp !== 'number') continue;
+    if (max < curTemp) {
+      max = curTemp;
+    }
+    if (min > curTemp) {
+      min = curTemp;
+    }
+  }
+  return max - min;
+};
+
+const amplitude = calcTempAplitude(temperatures);
+console.log(amplitude);
