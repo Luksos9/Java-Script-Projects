48683d492ee31b045cbbfd59e67e195f35bac544
Luksos9 <lukaszszumilas9@wp.pl>
2021-11-27 16:53:49 +0100

    finished the game

--- PigGame/script.js
+++ PigGame/script.js
@@ -40,15 +40,32 @@ const btnHold = document.querySelector('.btn--hold');
 
 const diceEl = document.querySelector('.dice');
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+// Global Scope variables
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
+
+// Starting conditions
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = currentScore;
+  current1El.textContent = currentScore;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+};
+
+// Set starting conditions (invoke init function)
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -87,7 +104,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // 2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       document
         .querySelector(`.player--${activePlayer}`)
@@ -101,3 +118,7 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+// Btn resseting the game
+
+btnNew.addEventListener('click', init);
